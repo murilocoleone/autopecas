ce5938b16ca8e7e0a8248d013d50f6660d159a14
Murilo <murilo.coleone.reis@gmail.com>
2013-02-27 01:36:55 -0300

    Adicionei atributo preco na classe Peca e metodos de teste para setPreco()

--- src/br/com/autopecas/model/Peca.java
+++ src/br/com/autopecas/model/Peca.java
@@ -3,6 +3,7 @@ package br.com.autopecas.model;
 public class Peca {
 	
 	private String nome;
+	private Double preco;
 	
 	public String getNome() {
 		return nome;
@@ -11,12 +12,27 @@ public class Peca {
 	public void setNome(String nome) { 
 		validaNome(nome);
 		this.nome = nome;
+	}	
+		
+
+	public void setPreco(Double preco) {
+		validaPreco(preco);		
+		this.preco = preco;
+	}
+		
+	public Double getPreco() {
+		return preco;
 	}
 	
 	private void validaNome(String nome){		
 		if(nome == null || nome.equals("")){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Valor de nome invalido");
 		}		
+	}
+	
+	private void validaPreco(Double valor) {
+		if(valor == null || valor <= 0)
+			throw new IllegalArgumentException("Valor peca invalido");
 	}
 
 }
--- srcTestes/br/com/autopecas/model/PecasTest.java
+++ srcTestes/br/com/autopecas/model/PecasTest.java
@@ -9,22 +9,61 @@ public class PecasTest {
 	private Peca peca = new Peca();
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void testSePecaLancaIllegalArgumentExceptionComNomeNull(){	
+	public void testSeLancaIllegalArgumentExceptionComNomePecaNull(){	
 		final String nome = null;
 		peca.setNome(nome);						
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
-	public void testSePecaLancaIllegalArgumentExceptionComValorNomeVazio(){
+	public void testSeLancaIllegalArgumentExceptionComValorNomePecaVazio(){
 		final String nome = "";
 		peca.setNome(nome);				
 	}		
 	
 	@Test
-	public void testValorNome(){
+	public void testValorNomePeca(){
 		final String nome = "Retrovisor";
 		peca.setNome(nome);
 		assertEquals("Valor de nome deveria ser Retrovisor", "Retrovisor", peca.getNome());		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSeLancaIllegalArgumentExceptionComPrecoPecaNull(){
+		final Double valor = null;
+		peca.setPreco(valor);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSeLancaIllegalArgumentExceptionComPrecoPecaZero(){
+		final Double valor = 0d;
+		peca.setPreco(valor);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSeLancaIllegalArgumentExceptionComPrecoPecaNegativo(){
+		final Double valor = -1d;
+		peca.setPreco(valor);
+	}
+	
+	@Test
+	public void testValorCinquentaParaPrecoPeca(){
+		final Double valor = 50d;
+		peca.setPreco(valor);
+		assertEquals("Deveria ser o valor 50", new Double(50), peca.getPreco());
+	}
+	
+	@Test
+	public void testValorCemPontoNoveParaPrecoPeca(){
+		final Double valor = 100.9d;
+		peca.setPreco(valor);
+		assertEquals("Deveria ser o valor 100.9", new Double(100.9), peca.getPreco());
+	}
+	
+	@Test
+	public void testValorDuzentosParaPrecoPeca(){
+		final Double valor = 200d;
+		peca.setPreco(valor);
+		assertEquals("Valor deveria ser 200", new Double(200), peca.getPreco());
 	}
 
 }
